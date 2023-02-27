56fc16c7e7684ea783cc0efbfe077c392bd4fe6b
Pelle Johnsen <pelle.johnsen@skybox.gg>
2023-02-27 08:32:22 +0100

    feat: use GH app for upgrade token workflow

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { javascript, typescript, vscode } from "projen";
+import { github, javascript, typescript, vscode } from "projen";
 const project = new typescript.TypeScriptAppProject({
   defaultReleaseBranch: "main",
   name: "tsapp",
@@ -9,6 +9,9 @@ const project = new typescript.TypeScriptAppProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp({}),
+  },
 });
 
 const code = new vscode.VsCode(project);
